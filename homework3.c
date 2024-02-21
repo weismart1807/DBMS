8a143ec25ce8bb43eb860b83bb0303e10d1272e9
weismart1807 <110816471+weismart1807@users.noreply.github.com>
2024-02-21 11:11:14 +0800

    HW3
    延續第一個作業的程式，增加支援類 Redis  List 的操作，指令包含 LPUSH，LPOP，RPUSH，RPOP，LLEN，LRANGE。

---
+++ homework3.c
@@ -0,0 +1,97 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "hw3.h"
+
+int main(){
+	NODE *first=NULL,*node;
+    printf("Please SET first one\n");
+
+	while(1){
+	setbuf(stdin,NULL);
+	char input[10];
+    printf("Press new bottom:\n");
+    scanf("%s",input);
+	getchar();//read the "enter"
+    if(strcmp(input,"LPUSH")==0){
+        LPUSH(&first);
+    }
+    else if(strcmp(input,"RPUSH")==0){
+        RPUSH(&first);
+    }
+    else if(strcmp(input,"LPOP")==0){
+        LPOP(&first);
+    }
+    else if(strcmp(input,"RPOP")==0){
+        RPOP(first);
+    }
+    else if(strcmp(input,"LLEN")==0){
+        int nodenumber = LLEN(first);
+        printf("%d",nodenumber);
+    }
+    else if(strcmp(input,"LRANGE")==0){
+        printList(&first);
+    }
+
+	//delete
+	else if(strcmp(input,"DELETE")==0){
+		char k[20];
+		printf("which key you want to delete:\n");
+		scanf("%s",k);
+		node=searchNode(first,k);
+		if(node!=NULL){
+			first=deleteNode(first,node);
+		}
+		else printf("No the key!\n\n");
+	}
+
+	else if(strcmp(input,"SET")==0){
+		RPUSH(&first);
+	}
+
+	//print
+	else if(strcmp(input,"GET")==0){
+		char k[20];
+		printf("which key you want to print:\n");
+		scanf("%s",k);
+		node=searchNode(first,k);
+		printf( "'key'\t'value'\n");
+		printf( "%s\t%s\n" , node->msg.key, node->msg.value);
+	}
+
+	//insert
+	else if(strcmp(input,"INSERT")==0){
+		char ip[20];
+		printf("which key you want to follow:");
+		scanf("%s",ip);
+		node=searchNode(first,ip);
+		if(node!=NULL){
+			insertNode(node);
+		}
+		else printf("No the key!\n\n");
+	}
+
+	//upgrade
+	else if(strcmp(input,"UPGRADE")==0){
+		char ip[20];
+		printf("Which key you want to rewrite:\n");
+		scanf("%s",ip);
+		node=searchNode(first,ip);
+		rewriteNode(node);
+	}
+
+	//free
+	else if(strcmp(input,"FREE")==0){
+		freeList(first);
+		printf("END\n");
+		break;
+	}
+
+	//bottom meaning
+	else if(strcmp(input,"BOTTOM")==0){
+		printf("SET,GET,LPUSH,RPUSH,LPOP,RPOP,LLEN,LRANGE,DELETE,INSERT,UPGRADE,FREE \n");
+	}
+	setbuf(stdin,NULL);
+	}
+
+	return 0;
+}
---
+++ hw3.h
@@ -0,0 +1,183 @@
+#ifndef LINKLIST_H_INCLUDED
+#define LINKLIST_H_INCLUDED
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+struct info
+{
+    char key[20];
+    char value[20];
+};
+typedef struct info info;
+struct node
+{
+    info msg;
+    struct node *next;
+    struct node *prev;
+};
+typedef struct node NODE;
+
+void printList(NODE **);
+void freeList(NODE *);
+void insertNode(NODE *);
+void rewriteNode(NODE *);
+NODE *searchNode(NODE *,char* );
+NODE *deleteNode(NODE *,NODE *);
+void kv(NODE *);
+void LPUSH(NODE **);
+void RPUSH(NODE **);
+void LPOP(NODE **);
+void RPOP(NODE *);
+int LLEN(NODE *);
+
+void kv(NODE *newnode){
+    printf("key value\n");
+    scanf("%s %s",newnode -> msg.key, newnode -> msg.value);
+}
+
+void LPUSH(NODE **first){
+    NODE* current = *first;
+    setbuf(stdin,NULL);
+    NODE *newNode = (NODE*)malloc(sizeof(NODE));
+    kv(newNode);
+
+    if (*first == NULL) {
+        *first = newNode;
+        newNode->next = NULL;
+        newNode->prev = NULL;
+    } else {
+        newNode->next = current;
+        newNode->prev = NULL;
+        (*first)->prev = newNode;
+        *first = newNode;
+    }
+}
+
+void RPUSH(NODE **first){
+    NODE *newNode = (NODE*)malloc(sizeof(NODE));
+    kv(newNode);
+
+    if (*first == NULL) {
+        *first = newNode;
+        newNode->next = NULL;
+        newNode->prev = NULL;
+    } else {
+        NODE* current = *first;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = newNode;
+        newNode->prev = current;
+    }
+}
+
+void LPOP(NODE **first){
+    if (*first == NULL) {
+        printf("NO LIST\n");
+    } else {
+        NODE* current = *first;
+        *first = current->next;
+        if(*first != NULL){
+            (*first)->prev = NULL;
+        }
+        free(current);
+    }
+}
+
+void RPOP(NODE *first){
+    if (first == NULL) {
+        printf("NO LIST\n");
+    } else {
+        NODE* current = first;
+        NODE* previous;
+        while (current->next != NULL) {
+            previous = current;
+            current = current->next;
+        }
+        previous->next = NULL;
+        free(current);
+    }
+}
+
+void printList(NODE **first){
+     NODE *node = *first;
+    if (*first == NULL )
+        printf("List is empty.\n" );
+
+    printf( "'key'\t'value'\n");
+    while (node != NULL){
+        printf("%s\t%s\n" , node->msg.key, node->msg.value);
+        node = node-> next;
+    }
+    printf(" \n" );
+}
+
+int LLEN(NODE *first){
+    NODE* current = first;
+    int sum = 0;
+    if(current->next == NULL)
+        return sum;
+    else{
+        while (current->next != NULL) {
+            current = current->next;
+            sum++;
+        }
+        sum++;
+        return sum;
+    }
+}
+
+NODE* deleteNode(NODE *first, NODE *point){
+     NODE *ptr=first;
+     if(first==NULL){
+        printf("Nothing to delete!\n");
+        return NULL;
+     }
+
+     else{
+        while(ptr->next!=point)
+            ptr=ptr->next;
+        ptr->next=point->next;
+     }
+     free(point);
+     return first;
+}
+
+void insertNode(NODE *node){
+     NODE *newnode;
+     newnode = (NODE*) malloc ( sizeof (NODE));
+     printf("'key'\t'value'\n");
+     scanf("%s %s",newnode->msg.key, newnode->msg.value);
+     newnode->next = node->next;
+     node->next=newnode;
+}
+
+void rewriteNode(NODE *node){
+    printf("'value'\n");
+    scanf("%s",node->msg.value);
+}
+
+void freeList(NODE *first){
+     NODE *current, *tmp;
+     current = first;
+     while (current)
+     {
+         tmp = current;
+         current = current-> next;
+         free (tmp);
+     }
+}
+
+NODE *searchNode(NODE *first, char *item){
+     NODE *node = first;
+     while (node!=NULL)
+     {
+         if (strcmp(node->msg.key,item)==0)
+             return node;
+         else
+             node = node-> next;
+     }
+     return NULL;
+}
+
+#endif // LINKLIST_H_INCLUDED
